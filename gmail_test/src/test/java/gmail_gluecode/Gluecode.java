b2837d1646ee20f6dadde1aa51f6e03e9192d5b4
verdentum <verdentum@DESKTOP-BRBAM2S>
2019-12-04 13:21:11 +0530

    changes done

--- gmail_test/src/test/java/gmail_gluecode/Gluecode.java
+++ gmail_test/src/test/java/gmail_gluecode/Gluecode.java
@@ -48,7 +48,7 @@ public void method(Scenario s) throws Exception
 	{
 		if(ghp.userid.isDisplayed())
 		{
-			ss.write("User id test      failed");
+			ss.write("User id test passed");
 		}
 		ghp.uerid();
 	}
